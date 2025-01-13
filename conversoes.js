26ffef74caac1af50a4a395178da13b8fd15847f
rafaelsilvarjs <rafael.eng99@gmail.com>
2025-01-13 16:27:56 -0300

    codigo var e convensoes

---
+++ conversoes.js
@@ -0,0 +1,16 @@
+// tipo de dados
+// boleanos 
+
+// conversão implícita
+
+const numero =456;
+const numeroString = "456";
+
+console.log(numero == numeroString)
+
+//string() para transformar number em string
+//number() para transformar string em number
+
+console.log(numero + Number( numeroString))
+
+// conversão implicita 
